ea0c3b39f764b7bc8f90ada5cc9c5af1b0a3042b
zhaodalai001 <zhaodalai001@ke.com>
2019-04-10 21:38:22 +0800

    852

--- leetcode/c/peak_index_in_a_mountain_array_852_binary_search.c
+++ leetcode/c/peak_index_in_a_mountain_array_852_binary_search.c
@@ -3,14 +3,21 @@
 int peakIndexInMountainArray(int* A, int ASize){
     int l = 0;
     int h = ASize;
-    while(l<=h){
-        int half = (l+h)/2;
-        if(*(A+l) < *(A+half)){
+    while( l < h ){
+        int half = (l+h) >> 1;
 
+        if(*(A+half-1) > *(A + half) && *(A + half + 1 ) < *(A + half)){
+            h = half;
+            continue;
         }
+        if( *(A + half -1 ) < *(A +half) && *(A + half +1) > *(A + half) ){
+            l = half;
+            continue;
+        }
+        return half;
     }
+    return 0;
 }
-
 int main(){
     int a[3] = {0,1,0};
     printf("%d \n", peakIndexInMountainArray(a , 3));
